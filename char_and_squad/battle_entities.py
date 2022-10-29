a4226d690f2cf2d449be668d03191c03080492a8
JNeto <joaomendes15@gmail.com>
2022-10-29 20:31:12 +0100

    Chars and Squads added, no bugs for fixing

---
+++ char_and_squad/battle_entities.py
@@ -0,0 +1,37 @@
+class Character:
+
+    def __init__(self, name, health, mana, armor, weapon_dmg, initiative):
+        self.name = name
+        self.health = health
+        self.mana = mana
+        self.armor = armor
+        self.weapon_dmg = weapon_dmg
+        self.initiative = initiative
+
+    def is_dead(self):
+        return self.health <= 0
+
+    def to_string(self) -> str:
+        txt: str = f"NAME: {self.name} | INITIATIVE: {self.initiative} \n" \
+                   f"HP: {self.health} | MANA: {self.mana} | ARMOR: {self.armor} | W_DMG: {self.weapon_dmg}\n" \
+                   f"is dead: {self.is_dead()}"
+        return txt
+
+
+class Squad:
+
+    def __init__(self, squad_name: str, list_of_char: list[Character]):
+        self.squad_name = squad_name
+        self.list_of_char = list_of_char
+
+    def to_string(self):
+        width = 45
+        text: str = "="*width + "\n" + f"Squad: {self.squad_name} \n" + "-"*width + "\n"
+        for i in range(0, len(self.list_of_char)):
+            text += self.list_of_char[i].to_string()
+            if i != len(self.list_of_char) - 1:
+                text += "\n" + "-"*width + "\n"
+            else:
+                text += "\n"
+        text += "="*width
+        return text
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from char_and_squad.battle_entities import Character, Squad
+
+squad1 = Squad("player squad", [Character("Joao", 10, 5, 5, 5, 10), Character("Dani", 10, 5, 5, 5, 10), Character("Lulu", 0, 5, 5, 5, 10)])
+squad2 = Squad("enemy squad", [Character("Monstrinho", 10, 5, 5, 5, 10), Character("Lobao", 10, 5, 5, 5, 10)])
+
+print(squad1.to_string())
+print()
+print()
+print(squad2.to_string())
+
+
