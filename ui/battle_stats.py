be3e93f5444fae0e53ce3bc7cc6f513d64028c52
JNetoGH <joaomendes15@gmail.com>
2022-10-30 16:52:13 +0000

    visual changes only

--- ui/battle_stats.py
+++ ui/battle_stats.py
@@ -2,30 +2,42 @@
 from char_and_squad.battle_entities import Character
 
 # VARS
-general_length = 24
+general_line_length = 24
+
+# DOUBLE LINES
+general_wall = "║"
+general_line = "═"
+general_up_left_corner = "╔"
+general_bottom_left_corner = "╚"
+general_up_right_corner = "╗"
+general_bottom_right_corner = "╝"
+
+# LIGHT SINGLE LINES
+general_light_line = "─"
+general_light_line_left_connector = "╟"
+general_light_line_right_connector = "╢"
 
 
-# main function
 def inspect_stats(character: Character):
-    print("\n" + "=" * general_length)
-    print(get_centralized_stat_line(general_length, character.name))
-    print("=" * general_length)
-    print(get_centralized_stat_line(general_length, f"HP: {character.health}"))
-    print(get_centralized_stat_line(general_length, f"Attack: {character.weapon_dmg}"))
-    print(get_centralized_stat_line(general_length, f"Mana: {character.mana}"))
-    print(get_centralized_stat_line(general_length, f"Armor: {character.armor}"))
-    print(get_centralized_stat_line(general_length, f"Initiative: {character.initiative}"))
-    print("=" * general_length)
+    print(f"{general_up_left_corner}{(general_line * (general_line_length - 2))}{general_up_right_corner}")
+    print(get_centralized_stat_line(general_line_length, character.name, general_wall, general_wall))
+    print(f"{general_light_line_left_connector}{general_light_line * (general_line_length - 2)}{general_light_line_right_connector}")
+    print(get_centralized_stat_line(general_line_length, f"HP: {character.health}", general_wall, general_wall))
+    print(get_centralized_stat_line(general_line_length, f"Attack: {character.weapon_dmg}", general_wall, general_wall))
+    print(get_centralized_stat_line(general_line_length, f"Mana: {character.mana}", general_wall, general_wall))
+    print(get_centralized_stat_line(general_line_length, f"Armor: {character.armor}", general_wall, general_wall))
+    print(get_centralized_stat_line(general_line_length, f"Initiative: {character.initiative}", general_wall, general_wall))
+    print(f"{general_bottom_left_corner}{(general_line * (general_line_length - 2))}{general_bottom_right_corner}")
 
 
-def get_centralized_stat_line(line_length: int, msg: str) -> str:
+def get_centralized_stat_line(line_length: int, msg: str = "Default", wall1: str = " ", wall2: str = " ") -> str:
     is_msg_length_even = len(msg) % 2 == 0
     if is_msg_length_even:
-        return "|" + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + msg + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + "|"
+        return wall1 + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + msg + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + wall2
     # Adds an extra space "|" -> " |", e.g. len(msg) = 11, the division per 2 would be 5, not 5.5,
     # therefore, it would be, (line_length - 5) + msg + (line_length - 5), being discounted 10 for the msg instead o 11
     else:
-        return "|" + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + msg + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + " |"
+        return wall1 + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + msg + " " * (int((line_length / 2 - len(msg) / 2)) - 1) + " " + wall2
 
 
 """
